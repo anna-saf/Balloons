01d41dc03ccad17981fd4e9f242c9ac473285082
AnnaSaf <safonova.irage@gmail.com>
2024-07-06 13:20:06 +0300

    Добавила модуль спавна шаров

---
+++ Assets/GameResources/Features/BallonsSpawner/BallonFacade.cs
@@ -0,0 +1,16 @@
+﻿namespace Ballons.Features.BallonsSpawner
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// Фасад объекта шара
+    /// </summary>
+    public class BallonFacade : MonoBehaviour
+    {
+        ///Получить SpriteRenderer шара
+        public SpriteRenderer BallonSpriteRenderer => ballonSpriteRenderer;
+
+        [SerializeField]
+        protected SpriteRenderer ballonSpriteRenderer = default;
+    }
+}
---
+++ Assets/GameResources/Features/BallonsSpawner/BallonsSpawner.cs
@@ -0,0 +1,60 @@
+﻿namespace Ballons.Features.BallonsSpawner
+{
+    using Ballons.Features.GameSettings;
+    using Ballons.Features.GameSpeed;
+    using Ballons.Features.Utilities;
+    using System.Collections;
+    using UnityEngine;
+    using Zenject;
+
+    /// <summary>
+    /// Контроллер спавна шаров
+    /// </summary>
+    public class BallonsSpawner : MonoBehaviour
+    {
+        protected IBallonFactory ballonsFactory = default;
+        protected ISpritePositionSetter positionSetter = default;
+        protected float spawnSpeed = default;
+
+        protected Coroutine spawnCoroutine = default;
+
+        [Inject]
+        protected virtual void Construct(IBallonFactory ballonsFactory, ISpritePositionSetter positionSetter, GameSpeedController gameSpeedController, BallonSpawnSettings ballonSpawnSettings)
+        {
+            this.ballonsFactory = ballonsFactory;
+            this.positionSetter = positionSetter;
+            spawnSpeed = ballonSpawnSettings.SpawnInterval * gameSpeedController.CurrentSpeed;
+        }
+
+        /// <summary>
+        /// Запустить спавн шаров
+        /// </summary>
+        public virtual void StartSpawn() =>
+            spawnCoroutine = StartCoroutine(SpawnBallons());
+
+        /// <summary>
+        /// Остановить спавн шаров
+        /// </summary>
+        public virtual void StopSpawn() 
+        {
+            if(spawnCoroutine != null)
+            {
+                StopCoroutine(spawnCoroutine);
+                spawnCoroutine = null;
+            }
+        }
+
+        protected virtual IEnumerator SpawnBallons()
+        {
+            while (isActiveAndEnabled)
+            {
+                BallonFacade ballon = ballonsFactory.CreateObject();
+                positionSetter.SetPosition(ballon.transform,ballon.BallonSpriteRenderer);
+                yield return new WaitForSeconds(spawnSpeed);
+            }
+        }
+
+        protected virtual void OnDisable() =>
+            StopSpawn();
+    }
+}
