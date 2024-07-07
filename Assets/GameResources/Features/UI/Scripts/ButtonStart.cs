72a55b1918f2e0ce9511a1fdd923f3831a7908ef
AnnaSaf <safonova.irage@gmail.com>
2024-07-07 08:11:32 +0300

    Сверстала меню, добавила кнопку старта игры

---
+++ Assets/GameResources/Features/UI/Scripts/ButtonStart.cs
@@ -0,0 +1,20 @@
+﻿namespace Balloons.Features.Utilities
+{
+    using Balloons.Features.GlobalGameEvents;
+    using Zenject;
+
+    /// <summary>
+    /// Кнопка старта игры
+    /// </summary>
+    public class ButtonStart : AbstractButtonView
+    {
+        protected GlobalGameEvents globalGameEvents = default;
+
+        [Inject]
+        protected virtual void Construct(GlobalGameEvents globalGameEvents) =>
+            this.globalGameEvents = globalGameEvents;
+
+        protected override void Action() =>
+            globalGameEvents.StartGame();
+    }
+}
