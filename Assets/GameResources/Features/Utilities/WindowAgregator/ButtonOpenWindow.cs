b93840ce2f747236c2b7ac5e65dab63868cb15ba
AnnaSaf <safonova.irage@gmail.com>
2024-07-07 08:03:54 +0300

    Добавила простой контроллер для управления окнами

---
+++ Assets/GameResources/Features/Utilities/WindowAgregator/ButtonOpenWindow.cs
@@ -0,0 +1,23 @@
+﻿namespace Balloons.Features.Utilities
+{
+    using UnityEngine;
+    using Zenject;
+
+    /// <summary>
+    /// Кнопка открытия окна 
+    /// </summary>
+    public class ButtonOpenWindow : AbstractButtonView
+    {
+        [SerializeField]
+        protected WindowType targetWindow = default;
+
+        protected WindowAgregator windowAgregator = default;
+
+        [Inject]
+        protected virtual void Construct(WindowAgregator windowAgregator) =>
+            this.windowAgregator = windowAgregator;
+
+        protected override void Action() =>
+            windowAgregator.SetWindowActive(targetWindow);
+    }
+}
---
+++ Assets/GameResources/Features/Utilities/WindowAgregator/WindowType.cs
@@ -0,0 +1,12 @@
+﻿namespace Balloons.Features.Utilities
+{
+    /// <summary>
+    /// Перечисление окон приложения
+    /// </summary>
+    public enum WindowType
+    {
+        Menu,
+        Game, 
+        Pause
+    }
+}
