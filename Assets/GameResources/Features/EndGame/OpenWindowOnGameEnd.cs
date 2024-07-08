979ff9d1047c39946cfa433cc486a7df61698027
AnnaSaf <safonova.irage@gmail.com>
2024-07-08 20:31:24 +0300

    Добавила контроллеры, отрабатывающие в конце игры

---
+++ Assets/GameResources/Features/EndGame/OpenWindowOnGameEnd.cs
@@ -0,0 +1,29 @@
+﻿namespace Balloons.Features.EndGame
+{
+    using Balloons.Features.GlobalGameEvents;
+    using Balloons.Features.Utilities;
+
+    /// <summary>
+    /// Открыть окно при завершении игры
+    /// </summary>
+    public class OpenWindowOnGameEnd : GlobalGameEventsProvider
+    {
+        protected WindowType window = default;
+        protected bool needClose = false;
+
+        protected WindowAgregator windowAgregator = default;
+
+        protected OpenWindowOnGameEnd(GlobalGameEvents globalGameEvents,
+                                      WindowAgregator windowAgregator,
+                                      WindowType window,
+                                      bool needClose) : base(globalGameEvents)
+        {
+            this.window = window;
+            this.needClose = needClose;
+            this.windowAgregator = windowAgregator;
+        }
+
+        protected override void OnEndGame() =>
+            windowAgregator.SetWindowActive(window, needClose);
+    }
+}
