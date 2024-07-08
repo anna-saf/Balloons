7045024d0515230eb665bc051de099413175522c
AnnaSaf <safonova.irage@gmail.com>
2024-07-08 20:46:24 +0300

    Добавила свитчер объектов

---
+++ Assets/GameResources/Features/Utilities/UIUtilities/BaseObjectSwitcher.cs
@@ -0,0 +1,34 @@
+﻿namespace Balloons.Features.UIUtilities
+{
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    /// <summary>
+    /// Свитчер объектов
+    /// </summary>
+    public class BaseObjectSwitcher : MonoBehaviour
+    {
+        [SerializeField]
+        protected List<GameObject> enabledObjects = new List<GameObject>();
+        [SerializeField]
+        protected List<GameObject> disabledObject = new List<GameObject>();
+
+        /// <summary>
+        /// Поменять статус объектов в списках
+        /// </summary>
+        /// <param name="isOn"></param>
+        public virtual void SetStatus(bool isOn)
+        {
+            SetListStatus(enabledObjects, isOn);
+            SetListStatus(disabledObject, !isOn);
+        }
+
+        private void SetListStatus(List<GameObject> targetList, bool isOn)
+        {
+            foreach (GameObject listObject in targetList)
+            {
+                listObject.SetActive(isOn);
+            }
+        }
+    }
+}
---
+++ Assets/GameResources/Features/Utilities/UIUtilities/SwitchViewOnEnable.cs
@@ -0,0 +1,11 @@
+﻿namespace Balloons.Features.UIUtilities
+{
+    /// <summary>
+    /// Переключить вью на OnEnable
+    /// </summary>
+    public class SwitchViewOnEnable : BaseObjectSwitcher
+    {
+        protected virtual void OnEnable() =>
+            SetStatus(true);
+    }
+}
