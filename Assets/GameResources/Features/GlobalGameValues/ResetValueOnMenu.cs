995ec0fac2edda80d07c93cf08cd73e104a60379
AnnaSaf <safonova.irage@gmail.com>
2024-07-08 20:41:41 +0300

    Добавила сброс значений при переходе в меню

---
+++ Assets/GameResources/Features/GlobalGameValues/ResetValueOnMenu.cs
@@ -0,0 +1,20 @@
+﻿namespace Balloons.Features.GlobalGameEvents
+{
+    using Balloons.Features.Utilities;
+
+    /// <summary>
+    /// Сбросить значение при заходе в меню
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ResetValueOnMenu<T> : GlobalGameEventsProvider
+    {
+        protected GenericEventValue<T> genericEventValue = default;
+
+        protected ResetValueOnMenu(GlobalGameEvents globalGameEvents,
+                                  GenericEventValue<T> genericEventValue) : base(globalGameEvents) =>
+            this.genericEventValue = genericEventValue;
+
+        protected override void OnGoToMenu() =>
+            genericEventValue.SetDefaultValue();
+    }
+}
