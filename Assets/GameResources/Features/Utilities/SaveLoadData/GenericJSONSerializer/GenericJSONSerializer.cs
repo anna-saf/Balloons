ff5c659922cd688c4b202f04fb21bcd8123264b1
AnnaSaf <safonova.irage@gmail.com>
2024-07-10 19:29:19 +0300

    Добавила SaveLoad utility

---
+++ Assets/GameResources/Features/Utilities/SaveLoadData/GenericJSONSerializer/GenericJSONSerializer.cs
@@ -0,0 +1,39 @@
+﻿namespace Balloons.Features.Utilities
+{
+    using Newtonsoft.Json;
+    using System;
+    using UnityEngine;
+
+    /// <summary>
+    /// Преобразователь данных из json в T и обратно
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class GenericJSONSerializer<T>
+    {
+        /// <summary>
+        /// Сериализовать данные
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public string SerializeData(T data) =>
+            JsonConvert.SerializeObject(data);
+
+        /// <summary>
+        /// Десериализовать данные
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public T DeserializeData(string data)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<T>(data);
+            }
+            catch(Exception e)
+            {
+                Debug.Log("Ошибка десериализации данных " + e.Message + e.StackTrace);
+                return default;
+            }
+        }
+    }
+}
