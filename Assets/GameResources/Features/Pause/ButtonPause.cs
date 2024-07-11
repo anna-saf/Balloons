c56548b4ad648e31fc88b26a843c845cfa5954f9
AnnaSaf <safonova.irage@gmail.com>
2024-07-11 03:39:07 +0300

    Добавила контроллер паузы и кнопки перехода и выхода из паузы

---
+++ Assets/GameResources/Features/Pause/ButtonPause.cs
@@ -0,0 +1,21 @@
+﻿namespace Balloons.Features.PauseController
+{
+    using Balloons.Features.Utilities;
+    using Zenject;
+
+    /// <summary>
+    /// Кнопка паузы
+    /// </summary>
+    public class ButtonPause : AbstractButtonView
+    {
+        private PauseController _pauseController = default;
+
+        [Inject]
+        private void Construct(PauseController pauseController) =>
+            _pauseController = pauseController;
+
+        protected override void Action() =>
+            _pauseController.PauseGame();
+    }
+}
+
---
+++ Assets/GameResources/Features/Pause/PauseController.cs
@@ -0,0 +1,19 @@
+﻿namespace Balloons.Features.PauseController
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// Контроллер паузы/продолжения игры
+    /// </summary>
+    public class PauseController
+    {
+        protected const int PAUSE_GAME = 0;
+        protected const int CONTINUE_GAME = 1;
+
+        public void PauseGame() =>
+            Time.timeScale = PAUSE_GAME;
+
+        public void ContinueGame() =>
+            Time.timeScale = CONTINUE_GAME;
+    }
+}
